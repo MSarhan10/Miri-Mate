5056dcb3fb2bb6d33b36c53c23bd6dc17274b1c9
MSarhan10 <130199705+MSarhan10@users.noreply.github.com>
2026-03-10 03:40:11 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
-const CACHE_NAME = 'mirimate-v1';
+const CACHE_NAME = 'mirimate-v2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './manifest.json',
-    './icon.png'
+    './icon-192.png',
+    './icon-512.png',
+    './icon-maskable-192.png',
+    './icon-maskable-512.png'
 ];
 
 // Install — cache all app files
